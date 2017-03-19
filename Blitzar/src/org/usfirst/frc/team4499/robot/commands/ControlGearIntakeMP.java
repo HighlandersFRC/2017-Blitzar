1a1af71263a772ffdab6c8a9d9db81b2b80d896b
Apassingbutterfly <hempel-costello.simon20@libertycommon.org>
2017-03-18 20:35:19 -0600

    Initial GearIntakeMP

--- Blitzar/src/org/usfirst/frc/team4499/robot/commands/ControlGearIntakeMP.java
+++ Blitzar/src/org/usfirst/frc/team4499/robot/commands/ControlGearIntakeMP.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc.team4499.robot.commands;
 
+import org.usfirst.frc.team4499.robot.RobotMap;
+
+import com.ctre.CANTalon;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -14,19 +18,34 @@ public class ControlGearIntakeMP extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	//TODO Add soft limits
+    	RobotMap.gearIntakeRoller.changeControlMode(CANTalon.TalonControlMode.MotionMagic);
+    	RobotMap.gearIntakeRoller.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Relative);
+    	RobotMap.gearIntakeRoller.setEncPosition(0);
+    	RobotMap.gearIntakeRoller.setForwardSoftLimit();
+    	RobotMap.gearIntakeRoller.setReverseSoftLimit();
+    	RobotMap.gearIntakeRoller.setMotionMagicAcceleration(500);
+    	RobotMap.gearIntakeRoller.setMotionMagicCruiseVelocity(500);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	RobotMap.gearIntakeRoller.set();
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
+    	if(RobotMap.gearIntakeRoller.getEncPosition() == 0){
+    		return true;
+    	}
         return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	RobotMap.gearIntakeRoller.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
+    	RobotMap.gearIntakeRoller.set(0);
     }
 
     // Called when another command which requires one or more of the same
